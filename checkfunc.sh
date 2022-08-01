6881ae69ef5046054d6a6d4aa7fbf8d14a9099af
Paracanthurus <okubo@MacBook-Pro.local>
2022-08-02 06:33:14 +0900

    改行を残す

--- checkfunc.sh
+++ checkfunc.sh
@@ -1,3 +1,3 @@
 #!bin/sh
 
-nm ../$1/*.o | grep -v "_ft_" | grep -v "ltmp" | grep -v "EH_frame" | grep -v "lC" | grep -v ^$ > out/checkfunc.txt
+nm ../$1/*.o | grep -v "_ft_" | grep -v "ltmp" | grep -v "EH_frame" | grep -v "lC" > out/checkfunc.txt
